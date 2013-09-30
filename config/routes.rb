9e3d05689459ee916bc01ae511716691f7afacef
ahmedhawas <ahmedhawas7@gmail.com>
2013-09-30 16:05:54 -0400

    Making tests for the browsing project pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Crowdfunder::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  resources :projects
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
