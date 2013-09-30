2bda3c8ae78c0ac54b2b9c6d6e8b079e2397a672
ahmedhawas <ahmedhawas7@gmail.com>
2013-09-30 16:17:52 -0400

    Writing tests for navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Crowdfunder::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  root :to => "projects#index"
   resources :projects
 
   # Sample of regular route:
