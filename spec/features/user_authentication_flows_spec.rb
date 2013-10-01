116fecdbba09e05e92872e89c19ceb6d8dc6669a
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-01 16:56:17 -0400

    Writing tests for errors on user registration form

--- spec/features/user_authentication_flows_spec.rb
+++ spec/features/user_authentication_flows_spec.rb
@@ -46,6 +46,11 @@ describe "User Authentication" do
 
       page.should have_no_content("Account created")
       expect(page).to have_no_content("Account created")
+
+      # Should see "Try again" message on failure to register 
+      within(:css, ".alert") do
+        have_content("Try again")
+      end
     end
   end
 end
