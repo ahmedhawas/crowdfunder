e28edd662f39ded5ac232f4f0747f9ceaaeb4cd1
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-03 11:18:55 -0400

    Writing tests for my projects navigation

--- spec/requests/project_flows_spec.rb
+++ spec/requests/project_flows_spec.rb
@@ -56,7 +56,7 @@ describe "Project Listing" do
       click_link 'Project 1'
       page.should have_selector('.navbar ul li.active a', text: "Projects")
       expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
-
+      
     end    
   end
 end
