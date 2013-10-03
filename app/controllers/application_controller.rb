ae00962c6e0fbca3f0a0f98e3274c9380bf8c2d9
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-03 15:37:11 -0400

    Making tests for pagination pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :nav_state
 
-  def nav_state
-    # To be overwritten by controllers
-  end
 
   protected
   def not_authenticated
   	redirect_to new_session_path, :alert => "Please login first."
   end
+   def nav_state
+    # To be overwritten by controllers
+  end
+
 end
